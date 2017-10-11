441ae5aac8a96d99ee991b7368460cc8b114874a
Menglong TAN <tanmenglong@gmail.com>
2017-10-11 19:26:22 +0800

    简化代码

--- core/src/main/scala/com/crackcell/jiezi/segment/ForwardMMSegment.scala
+++ core/src/main/scala/com/crackcell/jiezi/segment/ForwardMMSegment.scala
@@ -16,57 +16,51 @@ class ForwardMMSegment(val dicts: TermDict*) extends Segment {
 
   override def parse(query: String): Result = {
     var i = 0
+    var start = -1
+    var end = -1
+    var term: Option[Term] = None
+    var word: String = null
     val terms = new ArrayBuffer[Term]()
-    val enBuff = new StringBuffer()
-    val numBuff = new StringBuffer()
     while (i < query.length) {
-      var term: Option[Term] = None
-      var ch = query.charAt(i)
-
-      // 基于字典查找
+      val ch = query.charAt(i)
+      word = String.valueOf(ch)
       if (isStartChar(ch)) {
         term = findLongestHeadWord(query.substring(i, query.length))
         if (term.isDefined) {
-          if (term.get.getWord().length == 1 && StringUtils.isEnglishWord(term.get.getWord().charAt(0))) {
+          word = term.get.getWord()
+          if (word.length == 1 &&
+              (StringUtils.isEnglishWord(word.charAt(0)) || StringUtils.isDigit(word.charAt(0)))) {
             term = None
-          } else if (term.get.getWord().length == 1 && StringUtils.isNumber(term.get.getWord().charAt(0))) {
-            term = None
+            if (start == -1)
+              start = i
+            else
+              end = i
           } else {
-            i = i + term.get.getWord().length
             terms.append(term.get)
-            if (enBuff.length() != 0) {
-              terms.append(new Term(word = enBuff.toString, natures = Array(new Nature("en"))))
-              enBuff.delete(0, enBuff.length())
-            }
-            if (numBuff.length() != 0) {
-              terms.append(new Term(word = numBuff.toString, natures = Array(new Nature("m"))))
-              numBuff.delete(0, numBuff.length())
+            if (start != -1) {
+              val nature =
+                if (StringUtils.isEnglishWord(query.charAt(start))) "en"
+                else if (StringUtils.isNumber(query.charAt(start))) "m"
+                else throw new WordsegException("invalid stat")
+              terms.append(new Term(word = query.substring(start, end + 1), natures = Array(new Nature(nature))))
+              start = -1
+              end = -1
             }
           }
         }
       }
-
-      // 基于字典搜索失败，尝试使用策略补全term
-      if (term.isEmpty) {
-        if (StringUtils.isEnglishWord(ch)) {
-          enBuff.append(ch)
-          if (numBuff.length() != 0) {
-            terms.append(new Term(word = numBuff.toString, natures = Array(new Nature("m"))))
-            numBuff.delete(0, numBuff.length())
-          }
-        } else if (StringUtils.isNumber(ch)) {
-          numBuff.append(ch)
-          if (enBuff.length() != 0) {
-            terms.append(new Term(word = enBuff.toString, natures = Array(new Nature("en"))))
-            enBuff.delete(0, enBuff.length())
-          }
-        }
-        i = i + 1
-      }
+      i = i + word.length
     }
 
-    if (enBuff.length() != 0) terms.append(new Term(word = enBuff.toString, natures = Array(new Nature("en"))))
-    else if (numBuff.length() != 0) terms.append(new Term(word = numBuff.toString, natures = Array(new Nature("m"))))
+    if (start != -1) {
+      val nature =
+        if (StringUtils.isEnglishWord(query.charAt(start)))
+          "en"
+        else if (StringUtils.isNumber(query.charAt(start)))
+          "m"
+        else throw new WordsegException("invalid stat")
+      terms.append(new Term(word = query.substring(start, end + 1), natures = Array(new Nature(nature))))
+    }
 
     new Result(terms = terms.toArray)
   }
