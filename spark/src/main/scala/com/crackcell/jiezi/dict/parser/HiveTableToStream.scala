7b581167e7b8bb10f760707e9bbee8ea378e92a9
Menglong TAN <tanmenglong@gmail.com>
2017-10-16 15:37:53 +0800

    完成从Hive加载词典

--- spark/src/main/scala/com/crackcell/jiezi/dict/parser/HiveTableToStream.scala
+++ spark/src/main/scala/com/crackcell/jiezi/dict/parser/HiveTableToStream.scala
@@ -2,7 +2,9 @@ package com.crackcell.jiezi.dict.parser
 
 import java.io.ByteArrayInputStream
 
+import com.crackcell.jiezi.WordsegException
 import org.apache.commons.logging.LogFactory
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Row, SparkSession}
 
 /**
@@ -27,13 +29,24 @@ class HiveTableToStream extends PathToStream {
     }
 
     val df = spark.sql(s"SELECT * FROM ${table} WHERE ${conditions}")
+    val data = df.collect().map(rowToLine).filter(_.size > 0).mkString("\n")
 
-    new ByteArrayInputStream(df.collect().map(rowToLine).filter(_.size > 0).mkString("\n").getBytes())
+    new ByteArrayInputStream(data.getBytes())
   }
 
   private def rowToLine(row: Row): String = {
-    row.schema.fields.map { field =>
-      field.toString()
+    val schema = row.schema
+    schema.fields.filter(f => f.name != "name" && f.name != "version") .map { f =>
+      val index = schema.fieldIndex(f.name)
+      f.dataType match {
+        case StringType => row.getAs[String](f.name)
+        case IntegerType => row.getAs[Int](f.name).toString
+        case LongType => row.getAs[Long](f.name).toString
+        case FloatType => row.getAs[Float](f.name).toString
+        case DoubleType => row.getAs[Double](f.name).toString
+        case ArrayType(StringType, false) => row.getSeq[String](index).mkString(",")
+        case _ => throw new WordsegException(s"invalid type: ${f.name}, ${f.dataType}")
+      }
     }.mkString("\t")
   }
 }
---
+++ spark/src/test/scala/com/crackcell/jiezi/dict/parser/HiveTableToStreamTest.scala
@@ -0,0 +1,42 @@
+package com.crackcell.jiezi.dict.parser
+
+import com.crackcell.jiezi.segment.ForwardMMSegment
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+import org.scalatest.FunSuite
+
+/**
+  * 测试用例
+  *
+  * @author Menglong TAN
+  */
+class HiveTableToStreamTest extends FunSuite {
+  lazy val spark = SparkSession.builder().master("local[*]")
+      .enableHiveSupport().config("hive.exec.dynamic.partition.mode", "nonstrict")
+      .getOrCreate()
+  spark.sparkContext.setLogLevel("warn")
+
+  // 准备数据
+  spark.createDataFrame(
+    spark.sparkContext.parallelize(Seq(
+      Row("连衣裙", Array("infoword", "prop"), 10000L, "default", "v1"),
+      Row("充电宝", Array("infoword"), 10000L, "default", "v1")
+    )),
+    StructType(Seq(
+      StructField("keyword", StringType, nullable = false),
+      StructField("nature_list", ArrayType(StringType, false), nullable = false),
+      StructField("frequency", LongType, nullable = false),
+      StructField("name", StringType, nullable = false),
+      StructField("version", StringType, nullable = false)
+    ))
+  ).createOrReplaceTempView("default")
+
+  val loader = new TermDictLoader(new HiveTableToStream)
+  val coreDict = loader.loadDict("default")
+  val segment = new ForwardMMSegment(coreDict)
+
+  test("Wordseg with default dict") {
+    segment.setHandleInvalid("skip").parse("17年全新时尚连衣裙").getTerms().foreach(println)
+  }
+
+}
